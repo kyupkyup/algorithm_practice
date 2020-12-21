7cc70886212a5ead9e0dd33082ed8be58acc8ddd
kyupkyup <rudduqdl@naver.com>
2020-12-21 23:30:33 +0900

    힙을 이용한 우선순위 큐 구현

--- ch7 추상데이터 타입/우선순위 큐.py
+++ ch7 추상데이터 타입/우선순위 큐.py
@@ -3,10 +3,38 @@
 # 트리가 수정될 때, 다시 트리를 구성하는 데 O(logN) 소요
 
 import heapq
-list1 = [4, 6, 8, 1]
-heapq.heapify(list1)
-print(list1)
 
-heapq.heappush(list1, -5)
-print(list1)
 
+class PriorityQueue(object):
+    def __init__(self):
+        self.queue = []
+        self._index = 0
+
+    def push(self,item, priority):
+        heapq.heappush(self.queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self.queue)[-1]
+
+
+class Item:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "Item({0!r})".format(self.name)
+
+
+def test_priority_queue():
+    q=PriorityQueue()
+    q.push(Item("test1"), 1)
+    q.push(Item("test2"), 4)
+    q.push(Item("test3"), 3)
+    q.push(Item("test4"), 2)
+
+    print(str(q.pop()))
+
+
+if __name__ == "__main__":
+    test_priority_queue()
