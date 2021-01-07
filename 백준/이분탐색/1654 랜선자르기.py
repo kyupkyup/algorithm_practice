83d09000e922ba8cd6e51eaa91c6d0bf5849a98d
kyupkyup <rudduqdl@naver.com>
2021-01-07 13:33:54 +0900

    이분탐색 공부

--- 백준/이분탐색/1654 랜선자르기.py
+++ 백준/이분탐색/1654 랜선자르기.py
@@ -2,3 +2,36 @@
 
 # 최대 랜선의 길이를 탐색
 # 가져온 랜선의 최대 길이를 구해서
+# 그 사이에서 이분탐색
+
+k, n = map(int, input().split(" "))
+arr = []
+for i in range(k):
+    arr.append(int(input()))
+
+def solution():
+    # 나누기를 해서 몫들을 모두 합침.
+    print(binary_search(n, 1, max(arr)))
+
+
+
+def binary_search(target, start, end):
+    mid = (start + end) // 2
+    if start > end:
+        return end
+
+    if divide(mid) >= target:
+        # 너무 작게 자른것, 더 크게 잘라야 함
+        return binary_search(target, mid+1, end)
+    else:
+        return binary_search(target, start, mid-1)
+
+def divide(value):
+    sum = 0
+    for i in arr:
+        if i >= value:
+            sum += i // value
+    return sum
+
+
+solution()
---
+++ 백준/이분탐색/2110 공유기 설치.py
@@ -0,0 +1,17 @@
+
+# 거리를 탐색하는 것
+# C 개 만큼 선택했을 때, 최소 거리
+# n 개 중 C 개 선택했을 때,
+
+n, c = map(int, input())
+arr = []
+for i in range(n):
+    arr.append(i)
+
+def solution():
+
+
+def binary_search():
+
+
+solution()
