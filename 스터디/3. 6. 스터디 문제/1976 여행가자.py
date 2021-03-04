c5125cd98739ff1e5d5330ce8ef7e7a50900ffe9
kyupkyup <rudduqdl@naver.com>
2021-03-05 00:51:23 +0900

    스터디

--- 스터디/3. 6. 스터디 문제/1976 여행가자.py
+++ 스터디/3. 6. 스터디 문제/1976 여행가자.py
@@ -3,5 +3,39 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
+arr = [list(map(int, input().split(" "))) for _ in range(N)]
+spot = list(map(int, input().split(" ")))
+visited = [False] * (N+1)
+adj_list = [[] for _ in range(N+1)]
 
 for i in range(N):
+    for j in range(N):
+        if arr[j][i] == 1 or i == j:
+            adj_list[j].append(i)
+            adj_list[i].append(j)
+
+
+def solution():
+
+    for i in range(N):
+        for j in range(N):
+            if arr[j][i] == 1:
+                dfs(i, j)
+
+    for i in spot:
+        if not visited[i-1]:
+            print("NO")
+            return
+    print("YES")
+
+def dfs(i, j):
+    for v in adj_list[i]:
+        if not visited[v]:
+            visited[v] = True
+            dfs(v, j)
+    for k in adj_list[j]:
+        if not visited[k]:
+            visited[k] = True
+            dfs(i, k)
+
+solution()
