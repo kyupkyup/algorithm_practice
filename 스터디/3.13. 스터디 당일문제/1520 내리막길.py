4cd6e8459dcefa70044ebb3a273249fec6b9aadd
kyupkyup <rudduqdl@naver.com>
2021-03-15 22:13:50 +0900

    스터디

---
+++ 스터디/3.13. 스터디 당일문제/1520 내리막길.py
@@ -0,0 +1,51 @@
+import sys
+from _collections import deque
+
+input = sys.stdin.readline
+
+m, n = map(int, input().split(" "))
+
+arr = [list(map(int, input().split(" "))) for _ in range(m)]
+visited = [[0 for _ in range(n)] for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+count = 0
+
+
+def solution():
+
+
+    temp_cnt = 0
+    dq = deque([])
+
+    for k in range(4):
+        nx = 0 + dx[k]
+        ny = 0 + dy[k]
+
+        if 0 <= nx < n and 0 <= ny < m:
+            if arr[ny][nx] < arr[0][0]:
+                temp_cnt += 1
+
+    visited[0][0] = temp_cnt
+    dq.append([0, 0])
+
+    while dq:
+        temp_cnt = 0
+        i, j = dq.popleft()
+
+        for k in range(4):
+            nx = i + dx[k]
+            ny = j + dy[k]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if arr[ny][nx] < arr[j][i]:
+                    temp_cnt += 1
+                    visited[ny][nx] = max(visited[j][i] + temp_cnt - 1, visited[j][i])
+                    dq.append([nx, ny])
+
+    print(visited[m-1][n-1])
+    print(visited)
+#
+
+
+solution()
--- 스터디/3.13. 스터디 문제/1915 가장 큰 정사각형.py
+++ 스터디/3.13. 스터디 문제/1915 가장 큰 정사각형.py
@@ -10,11 +10,13 @@ def solution():
     ans_max = 0
     for j in range(1, n+1):
         for i in range(1, m+1):
+
             if arr[j-1][i-1] == 1:
+
                 ans[j][i] = min(ans[j-1][i], ans[j][i-1], ans[j-1][i-1])+1
+
             ans_max = max(ans[j][i], ans_max)
     print(ans_max ** 2)
-
 
 
 solution()
