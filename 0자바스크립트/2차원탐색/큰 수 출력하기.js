9b3b4408a13daadf583ee9c26df294530a59123b
kyupkyup <rudduqdl1@gmail.com>
2021-06-28 23:47:32 +0900

    210628

---
+++ 0자바스크립트/2차원탐색/큰 수 출력하기.js
@@ -0,0 +1,15 @@
+
+const solution = (N, arr) => {
+    for(let i = 0; i < arr.length-1; i++){
+        if(i === 0){
+            console.log(arr[i]);
+        }
+        else{
+            if(arr[i] < arr[i+1]){
+                console.log(arr[i+1])
+            }
+        }
+    }
+}
+
+solution(6, [7,3,9,5,6,12])
